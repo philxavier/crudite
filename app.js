0911216132161c7e5b11a50107eb040ffb247e35
Felipe <phil.sousa@gmail.com>
2018-07-20 10:56:33 -0700

    start of jquery functions

---
+++ app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  //attach event listeners to buttons
+
+  //create function stub for read/wite/delete
+    //research local storage
+
+  // $().on('click', function(){
+  //
+  // });
+
+  $().on('click', function(){
+
+  });
+
+});
